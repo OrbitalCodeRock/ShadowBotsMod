c4261566f714ad3d6048e5b9b550a74c6cda8f1d
Philosophbot <37189245+Philosophbot@users.noreply.github.com>
2019-09-28 20:24:35 -0500

    Add files via upload

---
+++ Scripts/Gravity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+	private bool Collision;
+	private Rigidbody2D rb;
+	// Update is called once per frame
+	void Update () {
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "G Shift") {
+			rb.gravityScale = -3;
+		}
+		if (other.tag == "G Return") {
+			rb.gravityScale = 3;
+		}
+	}
+}
---
+++ Scripts/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	public string Level = "Level_02"; 
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+			SceneManager.LoadScene(Level);
+	}
+}
